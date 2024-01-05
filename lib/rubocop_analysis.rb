a1a094c39b3171329566c33ce870647fc9463c82
Karl Heitmann <khf1988@gmail.com>
2024-01-05 12:31:31 -0300

    Created cli class

--- lib/rubocop_analysis.rb
+++ lib/rubocop_analysis.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "rubocop_analysis/version"
+require_relative "rubocop_analysis/cli"
 
 module RubocopAnalysis
   class Error < StandardError; end
---
+++ lib/rubocop_analysis/cli.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module RubocopAnalysis
+  # The CLI is a class responsible of handling all the command line interface
+  # logic.
+  class CLI
+    def initialize
+      puts "INITIALIZE Rubocop Analysis CLI"
+    end
+  end
+end
