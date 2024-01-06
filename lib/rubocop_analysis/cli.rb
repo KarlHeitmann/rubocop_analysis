b96df3a7f57fb0a59e02fcedb265e40cd89eedcb
Karl Heitmann <khf1988@gmail.com>
2024-01-06 11:07:13 -0300

    Refactor if into private methods

--- lib/rubocop_analysis/cli.rb
+++ lib/rubocop_analysis/cli.rb
@@ -20,26 +20,7 @@ module RubocopAnalysis
         action = gets.chomp
         break if action == "q"
 
-        if action == "1"
-          puts @result.nodes.map(&:as_hash)
-        elsif action == "2"
-          nodes = @result.filtered_nodes(@cops_filtered)
-          puts(nodes.map { "#{_1.path} # offenses: #{_1.offenses.count}" })
-          puts "\nTOTAL offended files: #{nodes.count}"
-        elsif action == "3"
-          results = []
-          nodes = @result.filtered_nodes([])
-          nodes.each do |node|
-            node.offenses.each do |offense|
-              results << offense.class unless results.include? offense.class
-            end
-          end
-          puts((1..results.count).to_a.zip(results).map { "#{_1[0]} - #{_1[1]}" })
-          puts "Select an option"
-          choice = 0
-          choice = gets.chomp.to_i until (choice.positive? && choice <= results.count)
-          @cops_filtered << results[choice - 1] # TODO: Add toggle, if the choice made by user already is in @cops_filtered, remove it
-        end
+        send(handle_main_menu[action])
       end
     end
 
@@ -53,6 +34,42 @@ module RubocopAnalysis
         (3) View Offenses
         (q)uit
       MAIN_MENU
+    end
+
+    def handle_main_menu
+      {
+        "1" => :show_all_files,
+        "2" => :show_offended_files,
+        "3" => :view_offenses,
+        "q" => :quit,
+      }
+    end
+
+    def show_all_files
+      puts @result.nodes.map(&:as_hash)
+    end
+
+    def show_offended_files
+      # TODO: Add a nested menu here, so the user can type something and filter files that match the typed path.
+      # The filters should be stored on instance variables
+      nodes = @result.filtered_nodes(@cops_filtered)
+      puts(nodes.map { "#{_1.path} # offenses: #{_1.offenses.count}" })
+      puts "\nTOTAL offended files: #{nodes.count}"
+    end
+
+    def view_offenses
+      results = []
+      @result.filtered_nodes([]).each do |node|
+        node.offenses.each do |offense|
+          results << offense.class unless results.include? offense.class
+        end
+      end
+      puts((1..results.count).to_a.zip(results).map { "#{_1[0]} - #{_1[1]}" })
+      puts "Select an option"
+      choice = 0
+      choice = gets.chomp.to_i until (choice.positive? && choice <= results.count)
+      selected_class = results[choice - 1]
+      @cops_filtered << results[choice - 1] # TODO: Add toggle, if the choice made by user already is in @cops_filtered, remove it
     end
   end
 end
